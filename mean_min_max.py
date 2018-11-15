931349b68fc1c4c052ff5b338e3e1a378fd4fd61
AlexRookie <alessandro.antonucci@unitn.it>
2018-11-15 11:39:09 +0100

    Fixed capital in labes

--- mean_min_max.py
+++ mean_min_max.py
@@ -9,13 +9,13 @@ axes1 = fig.add_subplot(1, 3, 1)
 axes2 = fig.add_subplot(1, 3, 2)
 axes3 = fig.add_subplot(1, 3, 3)
 
-axes1.set_ylabel('average')
+axes1.set_ylabel('Average')
 axes1.plot(numpy.mean(data, axis=0))
 
-axes2.set_ylabel('max')
+axes2.set_ylabel('Max')
 axes2.plot(numpy.max(data, axis=0))
 
-axes3.set_ylabel('min')
+axes3.set_ylabel('Min')
 axes3.plot(numpy.min(data, axis=0))
 
 fig.tight_layout()
