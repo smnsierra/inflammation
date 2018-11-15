ea508dc2c3bed9722e1680a1258898ceca60b11f
simone <smn.sierra@gmail.com>
2018-11-15 09:48:56 +0100

    Chaged fig size.

--- mean_min_max.py
+++ mean_min_max.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot
 # Add data file
 data = numpy.loadtxt(fname='data/inflammation-01.csv', delimiter=',')
 
-fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
+fig = matplotlib.pyplot.figure(figsize=(8.0, 4.0))
 
 axes1 = fig.add_subplot(1, 3, 1)
 axes2 = fig.add_subplot(1, 3, 2)
